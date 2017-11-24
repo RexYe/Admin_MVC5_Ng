b6722d3362ef9345f1d7eabe8ec138507c49bdec
Rex Ye <894450343@qq.com>
2017-11-24 18:04:18 +0800

    add angular

---
+++ admin/Angular-webpack-cli/README.md
@@ -0,0 +1,10 @@
+# Angular-webpack-cli
+
+#### 使用方法：
+git clone XXX;
+
+npm install; 安装依赖
+
+npm start;启动服务
+
+npm run build;构建项目
---
+++ admin/Angular-webpack-cli/src/controller/pageDController.js
@@ -0,0 +1,42 @@
+import tpl1 from '../views/DIALOG1TMPL.html'
+import dialogCtr from '../controller/dialogController'
+
+export default class pageDController {
+    constructor ($interval,httpService,utils) {
+        this.httpservice = httpService;
+        this.ut = utils;
+        this.activated = true;
+    };
+    submit () {
+        //console.log(this.query);
+        var myInit = {
+            method: 'POST',
+            body:"login_name="+this.query.userName+"&password="+this.query.password,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+        };
+       fetch('http://localhost:52273/api/values/userlogin2',myInit)
+       .then(function(response) {
+            return response.json();
+        })
+       .then(function(data) {
+            //console.log('data',data);
+            if(data[0] == null){
+                console.log('fail');
+                $location.path('../views/pageC.html');
+            }
+            else{
+                console.log('success');
+            }
+        })
+       .catch(function(e) {
+            console.log("Oops, error");
+        });
+    };
+    //显示密码绑定事件
+    toggleActivation () {
+
+    };
+}
+pageDController.$inject = ['$interval','httpService','utils'];
